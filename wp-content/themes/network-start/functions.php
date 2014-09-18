b83dd67bd31ecc8021c65cedc60eb42104f3e310
Pea <pea@misfist.com>
2014-09-17 21:09:57 -0400

    Updated function to display network-wide posts.

--- wp-content/themes/network-start/functions.php
+++ wp-content/themes/network-start/functions.php
@@ -377,51 +377,6 @@ function glocal_network_glocal_network_home_category() {
 	return $homeheading;
 }
 
-// Return the header entered in theme customization
-function glocal_network_home_header() {
-	$homeheading = get_option("post_heading", "Lastest");
-	return $homeheading;
-}
-
-
-/************* Get Global Navigation from Site 1 *****************/
-
-function glocal_network_navigation() {
-
-	//store the current blog_id being viewed
-	global $blog_id;
-	$current_blog_id = $blog_id;
-
-	//switch to the main blog which will have an id of 1
-	switch_to_blog(1);
-
-	//output the WordPress navigation menu
-	$glocal_network_nav = bones_main_nav();
-
-	//switch back to the current blog being viewed
-	switch_to_blog($current_blog_id);
-
-	return $glocal_network_nav;
-}
-
-/************* Get Custom Header Images from Other Sites *****************/
-
-function glocal_network_get_site_image($site_id) {
-	//store the current blog_id being viewed
-	global $blog_id;
-	$current_blog_id = $blog_id;
-
-	//switch to the main blog designated in $site_id
-	switch_to_blog($site_id);
-
-	$site_image = get_custom_header();
-
-	//switch back to the current blog being viewed
-	switch_to_blog($current_blog_id);
-
-	return $site_image->thumbnail_url;
-}
-
 /************* Add Slug to Body Class *****************/
 
 // Add specific CSS class by filter
@@ -475,31 +430,79 @@ function remove_theme_customization_glocal_network() {
 
 }
 
-/************* Get Recent Posts for All Network Sites *****************/
-// TODOs:
-// Add Sorting
-// Add arguments
-// Write function to render posts
-// Write shortcode to display render posts
+/************* Return the header entered in theme customization *****************/
 
-function network_posts() {
+function glocal_network_home_header() {
+	$homeheading = get_option("post_heading", "Lastest");
+	return $homeheading;
+}
+
+/************* Get Global Navigation from Site 1 *****************/
+
+function glocal_network_navigation($mainsite = 1) { //main site id defaulted to 1
+
+	//store the current blog_id being viewed
+	global $blog_id;
+	$current_blog_id = $blog_id;
+
+	//switch to the main blog which will usually have an id of 1
+	switch_to_blog($mainsite);
+
+	//output the WordPress navigation menu
+	$glocal_network_nav = bones_main_nav();
+
+	//switch back to the current blog being viewed
+	switch_to_blog($current_blog_id);
+
+	return $glocal_network_nav;
+}
+
+/************* Get Custom Header Images from Other Sites *****************/
+
+function glocal_network_get_site_image($siteid = 1) {
+	//store the current blog_id being viewed
+	global $blog_id;
+	$current_blog_id = $blog_id;
+
+	//switch to the site for which you want image
+	switch_to_blog($siteid);
+
+	$site_image = get_custom_header();
+
+	//switch back to the current blog being viewed
+	switch_to_blog($current_blog_id);
+
+	return $site_image->thumbnail_url;
+}
+
+/************* Get Recent Posts for All Network Sites *****************/
+
+// TODOs:
+	// Add Sorting
+	// Add arguments
+	// Write function to render posts
+	// Write shortcode to display render posts
+	// Move to plugin
+
+function network_posts($sortorder = 'date', $postcategory = '') {
 
 	$sites = wp_get_sites(); // TODO: Add argument to exclude main site (site 1)
 
-	foreach ($sites as $site) {
+	foreach ($sites as $site) { // Loop through network sites
+
 		$site_id = $site['blog_id']; // Variable to hold site ID
 
 		$sitedetails = get_blog_details($site_id);
-
-		if(function_exists('glocal_network_get_site_image')) { 
+ 
+		if(function_exists('glocal_network_get_site_image')) { // Function gets network-wide custom headers
 			$site_header = glocal_network_get_site_image($site_id);
 		}
 
 		$network_content['site-' . $site_id] = array(
-			'blog_id' => $site_id,
-			'blogname' => $sitedetails->blogname,
-			'siteurl' => $sitedetails->siteurl,
-			'site_header' => $site_header,
+			'blog_id' => $site_id,  // Put site ID into array
+			'blogname' => $sitedetails->blogname,  // Put site name into array
+			'siteurl' => $sitedetails->siteurl,  // Put site URL into array
+			'site_header' => $site_header,  // Put site custom header into array
 		);
 	    
 	    // Begin switch
@@ -511,11 +514,11 @@ function network_posts() {
 
 				$network_content['site-' . $site_id]['posts'][$recent['guid']] = array(
 					'ID' => $recent['ID'], // Integer value
-					'post_title' => $recent['post_title'],
-					'permalink' => get_permalink( $recent['ID'] ),
-					'post_content' => $recent['post_content'],
-					'post_date' => $recent['post_date'],
-					'post_author' => get_the_author_meta( 'display_name', $recent['post_author'] ),
+					'post_title' => $recent['post_title'], // Put post title into array
+					'permalink' => get_permalink( $recent['ID'] ), // Put post permalink (URL) into array
+					'post_content' => $recent['post_content'], // Put post content into array
+					'post_date' => $recent['post_date'], // Put post date into array
+					'post_author' => get_the_author_meta( 'display_name', $recent['post_author'] ), // Put post author into array
 					);
 
 				if(wp_get_attachment_url( get_post_thumbnail_id( $recent['ID'] ) )) {
@@ -526,8 +529,9 @@ function network_posts() {
 				
 				// TODO: Get link to author profile
 
-				$post_categories = wp_get_post_categories( $recent['ID'] );				
-				foreach($post_categories as $c){
+				$post_categories = wp_get_post_categories( $recent['ID'] );
+
+				foreach($post_categories as $c) {
 					$cat = get_category( $c );
 
 					$network_content['site-' . $site_id]['posts'][$recent['guid']]['post_categories'][$cat->slug] = array(
@@ -547,28 +551,32 @@ function network_posts() {
 			
 			foreach ($site_detail['posts'] as $post => $post_detail) {
 				
-				foreach ($post_detail as $key => $value) {
+				foreach ($post_detail as $key => $value) { // Loop through posts
 
 					$post_date = $post_detail['post_date'];
 
 					$network_posts[$post_date] = array(
-						'post_id' => $post_detail['ID'],
-						'post_title' => $post_detail['post_title'],
-						'post_date' => $post_detail['post_date'],
-						'post_author' => $post_detail['post_author'],
-						'post_content' => $post_detail['post_content'],
-						'permalink' => $post_detail['permalink'],
-						'post_thumbnail' => $post_detail['post_thumbnail'],
+						'post_id' => $post_detail['ID'], // Put post ID into array
+						'post_title' => $post_detail['post_title'], // Put post title into array
+						'post_date' => $post_detail['post_date'], // Put post date into array
+						'post_author' => $post_detail['post_author'], // Put post author into array
+						'post_content' => $post_detail['post_content'], // Put post content into array
+						'permalink' => $post_detail['permalink'], // Put post permalink (URL) into array
+						'post_thumbnail' => $post_detail['post_thumbnail'], // Put post thumbnail into array
+						'blog_id' => $site,
+						'blogname' => $site_detail['blogname'],
+						'siteurl' => $site_detail['siteurl'],
+						'site_header' => $site_detail['site_header'],
 					);
 
 					foreach ($post_detail['post_categories'] as $cats => $cat_detail) {
 						
-						foreach ($cat_detail as $catkey => $catvalue) {
+						foreach ($cat_detail as $catkey => $catvalue) { // Loop through post categories
 
-							$network_posts[$post_date]['post_categories'] = array(
-								'term_id' => $cat_detail['term_id'],
-								'name' => $cat_detail['name'],
-								'slug' => $cat_detail['slug'],
+							$network_posts[$post_date]['post_categories'][$cat_detail['term_id']] = array(
+								'term_id' => $cat_detail['term_id'], // Put post category ID into array
+								'name' => $cat_detail['name'], // Put post category name into array
+								'slug' => $cat_detail['slug'], // Put post category slud into array
 							);
 
 						}
@@ -581,11 +589,64 @@ function network_posts() {
 		
 	}
 
+	if($sortorder == 'date') {
+		arsort($network_posts);
+	}
+	
 	return $network_posts;
+	// return $network_content;
+
 }
 
+/************* Render Recent Posts for All Network Sites *****************/
+
+function display_network_posts() {
+
+	$network_posts = network_posts();
+
+	foreach($network_posts as $post) {
+		$post_id = $post['post_id'];
+		$post_title = $post['post_title'];
+		$post_author = $post['post_author'];
+		$post_content = $post['post_content'];
+		$blog_id = $post['blog_id'];
+		$blogname = $post['blogname'];
+		$siteurl = $post['siteurl'];
+		$site_header = $post['site_header'];
+		$site_details = "<a href='" . $siteurl . "'>" . $blogname . "</a>";
+		$post_excerpt = get_excerpt_by_id($post_id);
+
+		echo '<article id="site-id-post-id" class="site-id-post-id post" role="article">';
+		echo '	<header class="post-header" role="header">';
+		echo '		<h2 class="post-title" itemprop="headline">' . $post['post_title'] . '</h2>';
+		echo '		<div class="post-meta post-author">' . $post['post_author'] . '</div>';
+		echo '	</header>';
+		echo '	<div class="post-content" itemprop="articleBody">' . $post_excerpt . '</div>';
+		echo '	<footer class="post-footer">';
+		echo '		<div class="post-meta post-date"><time>' . date_i18n( get_option( 'date_format' ), $post['post_date'] ) . '</time></div>';
+		echo '		<div class="post-meta post-tags">';
+
+		foreach ($post['post_categories'] as $cats => $cat_detail) {
+
+			foreach ($cat_detail as $catkey => $catvalue) {
+
+				echo $cat_detail['term_id'];
+
+			}
+
+			// echo '<pre>';
+			// var_dump($post['post_categories']);
+			// echo '</pre>';
+
+		}
 
 
+		echo '      </div>';
+		echo '	</footer>';
+		echo '</article>';
 
+	}
+
+}
 
 ?>
