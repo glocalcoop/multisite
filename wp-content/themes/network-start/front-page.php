6bd4a9ded2fad6a736b3c171bc03ff109d0d35a8
Pea <pea@misfist.com>
2014-09-18 15:45:56 -0400

    Removed some unused crap.

--- wp-content/themes/network-start/front-page.php
+++ wp-content/themes/network-start/front-page.php
@@ -33,12 +33,16 @@
 
 		if ( is_plugin_active('events-manager/events-manager.php') ) { ?>
 
+			<?php get_template_part( 'partials/listings/listing', 'events' ); ?>
+
 		<?php } ?>
 
 
 		<?php // Recent Sites/Blogs
 
 		if(is_multisite()) { ?>
+
+			<?php get_template_part( 'partials/listings/listing', 'sites' ); ?>
 
 		<?php } ?>
 
@@ -47,60 +51,6 @@
 
 		dynamic_sidebar( 'home-modules' ); ?>
 
-
-
-
-
-
-		<?php get_template_part( 'partials/listing', 'posts' ); ?>
-
-		<?php get_template_part( 'partials/listing', 'events' ); ?>
-
-	<?php if (! is_multisite() ) { // Check to see if multisite is active. If not, display a recent posts and events module for this site. ?> 
-
-		<?php get_template_part( 'partials/content-home', 'posts' ); ?>
-
-		<?php get_template_part( 'partials/content-home', 'events' ); ?>
-
-		<?php get_template_part( 'partials/content-home', 'sites' ); ?>
-
-		<?php dynamic_sidebar( 'home-modules' ); ?>
-
-	<?php } else { // If multisite isn't enabled, show a recent posts module for the site ?>
-		
-
-		<article id="news-module" class="module row news">
-			<h2 class="module-heading"><a href="/news/">News</a></h2>
-			<ul class="news-list">
-
-				<?php get_template_part( 'partials/listing', 'posts' ); ?>
-
-			</ul>
-		</article>
-
-		<?php // Check to see if Events Manager is active. If not don't display this module.
-		if ( is_plugin_active('events-manager/events-manager.php') ) { ?>
-		<article id="events-module" class="module row events">
-			<h2 class="module-heading"><a href="/events/">Events</a></h2>
-			<ul class="events-list">
-				<?php
-				$events = EM_Events::output(array('limit'=>5, 
-					'format'=>'<li>
-					<h6 class="event-start">
-				        <time class="event-month" datetime="#M">#M</time>
-				        <time class="event-date" datetime="#j">#j</time>
-				        <time class="event-day" datetime="#D">#D</time>
-					</h6>
-					<h3 class="post-title event-title">#_EVENTLINK</h3>
-					</li>'));?>
-				<?php echo $events; ?>
-			</ul>
-		</article>
-		<?php } ?>
-
-		<?php dynamic_sidebar( 'home-modules' ); ?>
-
-	<?php } ?>
 
 	<?php
 	
